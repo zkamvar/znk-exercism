afa29f325982fd5829b94f05088833a3cf56097d
Zhian N. Kamvar <zkamvar@gmail.com>
2023-12-28 08:06:20 -0800

    first attempt

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -2,5 +2,6 @@ use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    use time::Duration;
+    start + Duration::seconds(1_000_000_000)
 }
