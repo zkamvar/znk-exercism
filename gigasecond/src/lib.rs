6ba7034a921e8d2469802a4e2b07aba9799ad1d7
Zhian N. Kamvar <zkamvar@gmail.com>
2023-12-28 08:08:13 -0800

    move use declaration to top

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,7 +1,7 @@
+use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    use time::Duration;
     start + Duration::seconds(1_000_000_000)
 }
