7223a16400bfac2a9f59127fbc3a7c4bfc898c66
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-06 17:48:47 -0800

    now I have a fencepost error

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -47,6 +47,7 @@ impl Clock<i32> {
 }
 
 fn parse_time(hours: i32, minutes: i32) -> Vec<i32> {
+    let mut new_hours = parse_hours(hours);
     let mut new_minutes = &minutes % 60;
     if minutes < 0 {
         new_minutes = 60 + new_minutes;
@@ -55,14 +56,19 @@ fn parse_time(hours: i32, minutes: i32) -> Vec<i32> {
         new_minutes = new_minutes % 60;
     }
 
-    let min_to_hours = minutes.abs() / 60;
-    let mut new_hours = &hours + min_to_hours;
-    new_hours = new_hours % 24;
+    let excess = minutes / 60;
+    new_hours = parse_hours(new_hours + excess);
+
+    return vec![new_hours, new_minutes];
+}
+
+fn parse_hours(hours: i32) -> i32 {
+    let mut new_hours = &hours % 24;
     if new_hours < 0 {
         new_hours = 24 + new_hours;
     }
     if new_hours > 23 {
         new_hours = new_hours % 24;
     }
-    return vec![new_hours, new_minutes];
+    new_hours
 }
