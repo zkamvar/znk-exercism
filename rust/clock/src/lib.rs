d931e50be01a4a412f604f2531802fb37e8d028e
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-06 18:09:17 -0800

    no improvment

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -48,24 +48,24 @@ impl Clock<i32> {
 
 fn parse_time(hours: i32, minutes: i32) -> Vec<i32> {
     let mut new_hours = parse_hours(hours);
-    let mut new_minutes = &minutes % 60;
+    let mut new_minutes = &minutes.abs() % 60;
     if minutes < 0 {
-        new_minutes = 60 + new_minutes;
+        new_minutes = 60 - new_minutes;
     }
     if new_minutes > 59 {
         new_minutes = new_minutes % 60;
     }
 
-    let excess = minutes / 60;
+    let excess = minutes / 60; // This is a floor operation.
     new_hours = parse_hours(new_hours + excess);
 
     return vec![new_hours, new_minutes];
 }
 
 fn parse_hours(hours: i32) -> i32 {
-    let mut new_hours = &hours % 24;
-    if new_hours < 0 {
-        new_hours = 24 + new_hours;
+    let mut new_hours = &hours.abs() % 24;
+    if hours < 0 {
+        new_hours = 24 - new_hours;
     }
     if new_hours > 23 {
         new_hours = new_hours % 24;
