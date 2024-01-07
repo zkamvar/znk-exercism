48c7b5186c13ed6c5dbf9c40c86e1a0522b6f560
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-06 16:51:38 -0800

    implement simple clock
    It passes the non-ignored tests, but does not pass the ignored tests

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,11 +1,42 @@
-pub struct Clock;
+use core::cmp::Ordering;
+use core::fmt::Debug;
+use std::fmt::Display;
 
-impl Clock {
+#[derive(Debug)]
+pub struct Clock<T> {
+    hours: T,
+    minutes: T,
+}
+
+impl<T: Display> ToString for Clock<T> {
+    fn to_string(&self) -> String {
+        let h = &self.hours;
+        let m = &self.minutes;
+        format!("{h:02}:{m:02}")
+    }
+}
+
+impl<T: Display + Debug> PartialOrd for Clock<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.to_string().partial_cmp(&other.to_string())
+    }
+}
+
+impl<T: Display + Debug> PartialEq for Clock<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_string() == other.to_string()
+    }
+}
+
+impl Clock<i32> {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Self { hours, minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        Self {
+            hours: self.hours,
+            minutes: self.minutes + minutes,
+        }
     }
 }
