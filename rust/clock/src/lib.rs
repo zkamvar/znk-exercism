ecd3aa7a94fe1c95535f8831a645d7b7380d3468
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-08 10:50:37 -0800

    implement minutes-only clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -5,66 +5,36 @@ use std::fmt::Display;
 
 #[derive(Debug, PartialOrd, PartialEq)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Display for Clock {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        let h = &self.hours;
-        let m = &self.minutes;
+        let h = get_hour(self.minutes).abs();
+        let m = get_minutes(self.minutes).abs();
         write!(f, "{h:02}:{m:02}")
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let time = parse_time(hours, minutes);
+        let h = hours.rem_euclid(24) * 60;
         Self {
-            hours: time[0],
-            minutes: time[1],
+            minutes: (h + minutes).rem_euclid(1440),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let time = parse_time(self.hours, &self.minutes + minutes);
         Self {
-            hours: time[0],
-            minutes: time[1],
+            minutes: (&self.minutes + minutes).rem_euclid(1440),
         }
     }
 }
 
-fn parse_time(hours: i32, minutes: i32) -> Vec<i32> {
-    let mut new_hours = parse_hours(hours);
-    let mut new_minutes = &minutes.abs() % 60;
-    let mut offset = 0;
-    if minutes < 0 {
-        new_minutes = 60 - new_minutes;
-        if new_minutes > 0 {
-            offset -= 1;
-        }
-    }
-    if new_minutes > 59 {
-        new_minutes = new_minutes % 60;
-        if new_minutes == 0 {
-            offset += 1;
-        }
-    }
-
-    let excess = minutes / 60;
-    new_hours = parse_hours(new_hours + excess + offset);
-
-    return vec![new_hours, new_minutes];
+fn get_hour(minutes: i32) -> i32 {
+    minutes.div_euclid(60).rem_euclid(24)
 }
 
-fn parse_hours(hours: i32) -> i32 {
-    let mut new_hours = &hours.abs() % 24;
-    if hours < 0 {
-        new_hours = 24 - new_hours;
-    }
-    if new_hours > 23 {
-        new_hours = new_hours % 24;
-    }
-    new_hours
+fn get_minutes(minutes: i32) -> i32 {
+    minutes.rem_euclid(60)
 }
