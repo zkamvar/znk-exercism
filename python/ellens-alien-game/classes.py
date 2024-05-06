414ec81864468a657d3b8bb9a2d267f8a3fc36ad
Zhian N. Kamvar <zkamvar@gmail.com>
2024-05-05 20:38:27 -0700

    [python] submit ellens alien game

--- python/ellens-alien-game/classes.py
+++ python/ellens-alien-game/classes.py
@@ -1,6 +1,5 @@
 """Solution to Ellen's Alien Game exercise."""
 
-
 class Alien:
     """Create an Alien object with location x_coordinate and y_coordinate.
 
@@ -18,8 +17,29 @@ class Alien:
     teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
     collision_detection(other): Implementation TBD.
     """
+    x_coordinate = 0
+    y_coordinate = 0
+    health = 3
+    total_aliens_created = 0
+    def __init__(self, ex, why):
+        self.x_coordinate = ex
+        self.y_coordinate = why
+        self.health = 3
+        Alien.total_aliens_created += 1
 
-    pass
+    def hit(self):
+        self.health -= 1
 
+    def is_alive(self):
+        return self.health > 0
+
+    def teleport(self, ex, why):
+        self.x_coordinate = ex
+        self.y_coordinate = why
+
+    def collision_detection(self, obj):
+        pass
 
 #TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
+def new_aliens_collection(starts):
+    return [Alien(pos[0], pos[1]) for pos in starts]
